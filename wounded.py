1a65a2ef2556590c4bbec57f13dea2f79117daa8
justin870206 <58811092+justin870206@users.noreply.github.com>
2019-12-14 00:58:42 +0800

    傷兵情報的檔案
    格式跟之前新聞的一模一樣

---
+++ wounded.py
@@ -0,0 +1,63 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class wounded():
+    '''
+    抓NBA傷兵情報
+
+    def get_news() 不用input
+    return: 一則新聞一個list[標題, 時間, 簡短內文, 新聞超連結網址, 圖片鏈結]
+            三個list裝在一個二維list裡面回傳
+    '''
+
+    def __init__(self):
+        response = requests.get("https://nba.udn.com/nba/cate/6754/6779")
+        soup = BeautifulSoup(response.text, 'html.parser')
+        attr = {'id' : 'news_list_body'}
+        news_tag = soup.find_all('div', attrs = attr)
+
+        for i in news_tag:
+            self.pa = str(i)
+
+        self.data = list()
+
+    def get_news(self):
+        count = 0
+        tend = 0
+        tiend = 0
+        pend = 0
+        hend = 0
+        piend = 0
+        
+        while count < 3:
+            tstart = self.pa.find('<h3>', tend)
+            tend = self.pa.find('</h3>', tstart)
+            title = self.pa[tstart + 4:tend]
+
+            tistart = self.pa.find('h24">', tiend)
+            tiend = self.pa.find('</b>', tistart)
+            time = self.pa[tistart + 5:tiend]
+
+            pstart = self.pa.find('<p>', pend)
+            pend = self.pa.find('</p>', pstart)
+            p = self.pa[pstart + 3:pend]
+            
+            hstart = self.pa.find('/nba/story/', hend)
+            hend = self.pa.find('">', hstart)
+            href = 'https://nba.udn.com' + self.pa[hstart:hend]
+
+            pistart = self.pa.find('data-src', piend)
+            piend = self.pa.find('/><', pistart)
+            html = self.pa[pistart + 10:piend - 1]
+
+            count += 1
+            self.data.append([title, time, p, href, html])
+
+        return self.data
+
+
+# 以下為試class的功能
+wounded = wounded()
+final = wounded.get_news()
+print(final)
