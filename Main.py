7ee029bde4e659a8d41689cb8b1a41b4df10279c
Nana2929 <58811089+Nana2929@users.noreply.github.com>
2019-12-14 17:18:23 +0800

    我開了LoginPage!
    吳宗勳login pageㄉ美編就交給你ㄌ

--- Main.py
+++ Main.py
@@ -148,7 +148,7 @@ class SportsLottery(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for page in (NewsPage, TeamPage, PersonalPage): # StartPage：測試用，之後會換掉
+        for page in (LoginPage, NewsPage, TeamPage, PersonalPage): # StartPage：測試用，之後會換掉
             page_name = page.__name__
             frame = page(parent=container, controller=self)
             self.frames[page_name] = frame # 存進dictionary
@@ -157,7 +157,7 @@ class SportsLottery(tk.Tk):
             # will be the one that is visible.
             frame.grid(row=1, column=0, sticky="nsew")
         # 預設開啟頁面為新聞頁
-        self.show_frame("NewsPage")
+        self.show_frame("LoginPage")
         
         
     def show_frame(self, page_name):
@@ -165,7 +165,44 @@ class SportsLottery(tk.Tk):
         frame = self.frames[page_name]
         frame.tkraise()
 
-# class LoginPage(tk.Frame):
+class LoginPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent) 
+        self.controller=controller
+        self.configure(width=250, height=700)
+        self.pack(side=BOTTOM, expand=TRUE)
+        self.canvas=tk.Canvas(self, height=1000, width=500, bg="plum2") 
+        # self.img=Image.open("NBALogo.gif")
+        # self.img=self.img.resize((200, 200), Image.ANTIALIAS) 
+        # self.img=ImageTk.PhotoImage(self.img)
+        # self.canvas.create_image(0, 0, anchor="nw", image=self.img)
+        self.canvas.pack(fill=BOTH,expand=Y)
+        self.canvas.configure(bg="misty rose")
+
+        f1=tkFont.Font(size=15, family="Didot")
+        self.l1=tk.Label(self.canvas, text="使用者名稱：", font=f1)
+        self.l2=tk.Label(self.canvas, text="密碼：", font=f1)
+        self.l1.pack(side=TOP, fill=X, padx=10, pady=10)
+        self.var_usr_name=tk.StringVar(self)
+        self.entry_usr_name=tk.Entry(self.canvas, textvariable=self.var_usr_name)
+        self.entry_usr_name.pack()
+        # 默認值
+        # var_usr_name.set("")
+        self.l2.pack(side=TOP,padx=20, pady=10) #fill=X, 
+
+        self.var_usr_pwd=tk.StringVar()
+        self.entry_usr_pwd=tk.Entry(self.canvas, textvariable=self.var_usr_pwd, show="*") 
+        self.entry_usr_pwd.pack(side=TOP, padx=10, pady=10)
+        # 以下login command之後要寫成判斷式並用configure結合
+        self.btn_login=tk.Button(self.canvas, text="Log in", font=f1, command=lambda: controller.show_frame("NewsPage"))
+        self.btn_login.pack(side=RIGHT, padx=10, pady=10)
+        self.btn_signup=tk.Button(self.canvas, text="Sign up", font=f1, command=self.usr_signup)
+        self.btn_signup.pack(side=RIGHT, padx=10, pady=10)
+
+    def usr_signup(self):
+        pass
+    
+    
 
 # NewsPage新聞頁
 class NewsPage(tk.Frame):
@@ -174,7 +211,7 @@ class NewsPage(tk.Frame):
         tk.Frame.__init__(self, parent) 
         self.controller=controller
         self.configure(bg="lemon chiffon",width=250, height=700)
-        self.pack(side=BOTTOM, expand=TRUE)
+        # self.pack(side=BOTTOM, expand=TRUE)
         # welcome page
         self.F1=tk.Frame(self,bg="misty rose",width=500, height=300)
         self.F1.pack(side=TOP, fill=BOTH,anchor=N)
